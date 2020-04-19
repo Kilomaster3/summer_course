9eebbf40ad085178ba391ff2fc4f2e69482a9f5a
Kilomaster_3 <hromalex.2011@gmail.com>
2020-04-19 12:16:30 +0300

    added ability for todo list, add todo items, and delete todo items

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_419_081_130) do
-  create_table 'todo_items', force: :cascade do |t|
-    t.string 'content'
-    t.integer 'todo_list_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['todo_list_id'], name: 'index_todo_items_on_todo_list_id'
+ActiveRecord::Schema.define(version: 2020_04_19_081130) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
-  create_table 'todo_lists', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'todo_items', 'todo_lists'
+  add_foreign_key "todo_items", "todo_lists"
 end
